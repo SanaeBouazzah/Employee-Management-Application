5ccbf9a3725638a167de8ec09c0900716b68a57d
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-22 14:32:51 +0200

    laravel-CRUD

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -26,29 +26,31 @@
           @csrf
           <div class="mb-3">
             <label for="name">Name:</label>
-            <input type="text" name="name" id="name" placeholder="Enter your name.." class="form-control">
+            <input type="text" name="name" id="name" placeholder="Enter your name.." class="form-control"
+            @error('name') is-invalid @enderror>
             @error('name')
-            <p class="valid-feedback">{{$message}}</p>
+            <p class="invalid-feedback">{{$message}}</p>
             @enderror
           </div>
           <div class="mb-3">
             <label for="email" class="form-control">Email:</label>
-            <input type="text" name="email" id="email" placeholder="Enter your email.." class="form-control">
+            <input type="text" name="email" id="email" placeholder="Enter your email.." class="form-control"
+            @error('email') is-invalid @enderror>
             @error('email')
-            <p class="valid-feedback">{{$message}}</p>
+            <p class="invalid-feedback">{{$message}}</p>
             @enderror
           </div>
           <div class="mb-3">
             <label for="address" class="form-control">Address:</label>
             <textarea type="text" name="address" id="address" placeholder="Enter your address.." class="form-control"></textarea>
             @error('address')
-            <p class="valid-feedback">{{$message}}</p>
+            <p class="invalid-feedback">{{$message}}</p>
             @enderror
           </div>
           <div class="mb-3">
             <input type="file" name="image" id="image" class="form-control">
             @error('image')
-            <p class="valid-feedback">{{$message}}</p>
+            <p class="invalid-feedback">{{$message}}</p>
             @enderror
           </div>
           <div class="my-4">
