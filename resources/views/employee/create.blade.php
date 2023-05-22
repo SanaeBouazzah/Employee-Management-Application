aeda3d02ab074439a19404cccb4095ac9e071793
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-22 14:23:27 +0200

    laravel-CRUD

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -22,7 +22,7 @@
     </div>
     <div class="card border-0 shadow-lg my-4">
       <div class="card-body">
-        <form action="" method="POST">
+        <form action="{{route('employees.store')}}" method="POST">
           <div class="mb-3">
             <label for="name">Name:</label>
             <input type="text" name="name" id="name" placeholder="Enter your name.." class="form-control">
