3e9892aa80f8acb42b252d73286b3abe9752666c
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-23 01:41:29 +0200

    laravel-CRUD

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -3,7 +3,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Simple CRUD Laravel</title>
+  <title>Gestion des Employees</title>
   <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
 </head>
 <body>
