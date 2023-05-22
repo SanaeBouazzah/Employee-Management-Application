2d97b9ed60f2e411af13086e68adceb771c77cf6
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-22 14:56:31 +0200

    laravel-CRUD

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -22,7 +22,7 @@
     </div>
     <div class="card border-0 shadow-lg my-4">
       <div class="card-body">
-        <form action="{{route('employees.store')}}" method="POST">
+        <form action="{{route('employees.store')}}" method="POST" enctype="multipart/form-data">
           @csrf
           <div class="mb-3">
             <label for="name" class="mb-2">Name:</label>
