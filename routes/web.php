3575b3476873e508ef1d7f8871af4b4821570ead
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-24 03:03:43 +0200

    laravel-CRUD

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EmployeeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [EmployeeController::class, 'index'])->name('employees.index');
 Route::get('/create', [EmployeeController::class, 'create'])->name('employees.create');
