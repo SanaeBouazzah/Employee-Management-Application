ff304ca0651aec0b98c45aa51746ab526a88ef72
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-24 02:25:01 +0200

    laravel-CRUD

--- resources/views/employee/list.blade.php
+++ resources/views/employee/list.blade.php
@@ -55,7 +55,7 @@
                   <td>
                     <a href="{{route('employees.edit', $employee->id)}}" class="btn btn-primary btn-sm">Edit</a>
                     <a href="" onclick="deleteEmployee({{$employee->id}})" class="btn btn-danger btn-sm">Delete</a>
-                    <form id="employee-edit-action-{{}}" action="{{route('employees.delete', $employee->id)}}" method="POST">
+                    <form id="employee-edit-action-{{$employee->id}}" action="{{route('employees.delete', $employee->id)}}" method="POST">
                       @csrf
                       @method('delete')
 
