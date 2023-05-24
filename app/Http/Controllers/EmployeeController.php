8e674719e598f3ab4e276c4474ffc2d9ba25c021
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-24 02:34:24 +0200

    laravel-CRUD

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -95,7 +95,7 @@ class EmployeeController extends Controller
       $employee = Employee::findOrFail($id);
       File::delete(public_path().'/uploads/employees/'.$employee->image);
       $employee->delete();
-      $request->session()->flash()
+      $request->session()->flash('success');
       return redirect()->route('employees.index');
     }
 }
