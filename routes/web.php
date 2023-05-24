b2eb5583c6acbd27a8cad6d17b4a3f53095ba0a0
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-24 03:00:36 +0200

    laravel-CRUD

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\EmployeeController;
 |
 */
 
-Route::get('/employees', [EmployeeController::class, 'index'])->name('employees.index');
+Route::get('/', [EmployeeController::class, 'index'])->name('employees.index');
 Route::get('/employees/create', [EmployeeController::class, 'create'])->name('employees.create');
 Route::post('/employees', [EmployeeController::class, 'store'])->name('employees.store');
 Route::get('/employees/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
