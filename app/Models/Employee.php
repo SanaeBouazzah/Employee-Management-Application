2fb979def74a904fcc01159ab0f966b55737ac6a
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-24 02:59:36 +0200

    laravel-CRUD

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
-    protected $fillable = [];
+    protected $fillable = ['name', 'email', 'address', 'image'];
 }
