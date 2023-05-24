bf561a78721cbc1d018071629192b718b9e6d7ca
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-24 03:03:27 +0200

    laravel-CRUD

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use App\Http\Controllers\EmployeeController;
 */
 
 Route::get('/', [EmployeeController::class, 'index'])->name('employees.index');
-Route::get('/employees/create', [EmployeeController::class, 'create'])->name('employees.create');
-Route::post('/employees', [EmployeeController::class, 'store'])->name('employees.store');
-Route::get('/employees/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
-Route::put('/employees/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
-Route::delete('/employees/{employee}', [EmployeeController::class, 'delete'])->name('employees.delete');
+Route::get('/create', [EmployeeController::class, 'create'])->name('employees.create');
+Route::post('/', [EmployeeController::class, 'store'])->name('employees.store');
+Route::get('/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
+Route::put('/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
+Route::delete('/{employee}', [EmployeeController::class, 'delete'])->name('employees.delete');
